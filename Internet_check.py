58638da8fe91c8b42ed236486b68ad7913fe0c78
zarar7576 <31075202+zarar7576@users.noreply.github.com>
2019-08-19 13:19:05 +0500

    Added a modifed headless script
    Added a modifed headless script

---
+++ Internet_check.py
@@ -0,0 +1,32 @@
+from urllib.request import urlopen
+from playsound import playsound
+import time
+
+def internet_on():
+    try:
+        response = urlopen('https://www.google.com/',timeout=1)
+        return True
+    except: 
+        return False
+
+connections=1
+##print("Connecton retry: "+str(connections),end="\r")
+
+
+while 1:
+    if not(internet_on()):
+        connections+=1
+        print("Connecton retry: "+str(connections),end="\r")
+    else:
+        break
+    
+print("\n")
+print(internet_on())
+
+
+playsound('internetfound.mp3')
+print("Internet connection found")
+
+time.sleep(10)
+
+quit()
